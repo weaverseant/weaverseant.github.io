d47ce3535455a3caced2184ae5e8e6a8cf074451
weaverseant <stweaver94@gmail.com>
2025-07-14 21:28:27 -0400

    Initial commit: Add gradebook HTML and JS files

---
+++ gradebook.js
@@ -0,0 +1,17 @@
+// TODO: Fetch data from the PostgreSQL database (to be implemented later)
+function fetchGradeData() {
+  // This function will query the PostgreSQL database and return grade data
+  console.log("Fetching grade data...");
+}
+
+// TODO: Populate the table with grade data
+function populateGradebook(data) {
+  // This function will take the fetched grade data and populate the table
+  console.log("Populating gradebook with data:", data);
+}
+
+// TODO: REMOVE THIS
+// Call the stubs to demonstrate the workflow
+const gradeData = fetchGradeData();
+populateGradebook(gradeData);
+// END REMOVE
